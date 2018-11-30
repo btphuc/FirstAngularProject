8840e9e8882c44ed43eb8b2b3d946495ec528d43
Phuc Bui <phuc.bui@scc.com>
2018-11-30 15:05:00 +0700

    recipe model

--- src/app/RecipeBook/RecipeList/recipelist.component.ts
+++ src/app/RecipeBook/RecipeList/recipelist.component.ts
@@ -1,11 +1,14 @@
 import {Component} from '@angular/core';
+import { Recipe } from '../recipe.model.ts'
 
 @Component({
   selector: 'app-recipe-list',
-  templateUrl: './recipelist.component.html'
+  templateUrl: '../recipelist.component.html'
 })
 
 export  class RecipelistComponent {
-  recipe = [];
+  recipes: Recipe[]  = [
+    new Recipe('A Test Recipe', 'This is a test recipe', 'https://api.norecipes.com/wp-content/uploads/2018/08/teriyaki-chicken-recipe_007.jpg')
+  ];
 
 }
